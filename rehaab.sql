0bb22797c8ef07dc14a487b2fea1e0faf05522c0
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2023-10-07 18:06:00 +0300

    Update rehaab.sql

--- rehaab.sql
+++ rehaab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 28, 2023 at 07:19 AM
+-- Generation Time: Oct 07, 2023 at 05:02 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -20,6 +20,52 @@ SET time_zone = "+00:00";
 --
 -- Database: `rehaab`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminpram`
+--
+
+CREATE TABLE `adminpram` (
+  `id` int(20) NOT NULL,
+  `NumOfSingleV` int(7) NOT NULL,
+  `NumOfDoubleV` int(7) NOT NULL,
+  `NumOfVehicles` int(7) NOT NULL,
+  `ReservationDur` int(3) NOT NULL,
+  `numOfVehiclesInSlot` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `adminpram`
+--
+
+INSERT INTO `adminpram` (`id`, `NumOfSingleV`, `NumOfDoubleV`, `NumOfVehicles`, `ReservationDur`, `numOfVehiclesInSlot`) VALUES
+(1, 7, 3, 10, 90, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id` int(20) NOT NULL,
+  `date` varchar(10) NOT NULL,
+  `time` varchar(6) NOT NULL,
+  `vehicleId` int(20) NOT NULL,
+  `drivingType` varchar(20) NOT NULL,
+  `driverGender` varchar(20) DEFAULT NULL,
+  `Status` varchar(20) NOT NULL,
+  `visitorId` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `date`, `time`, `vehicleId`, `drivingType`, `driverGender`, `Status`, `visitorId`) VALUES
+(1, '2023-10-23', '18:00', 1, 'Self driving', NULL, 'Confirmed', 40);
 
 -- --------------------------------------------------------
 
@@ -44,9 +90,51 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`ID`, `FirstName`, `LastName`, `Email`, `Password`, `Type`, `Status`) VALUES
 (40, 'Fatimah', 'alnaser', 'alnaserfatimah344@gmail.com', 'Fatimah123!', 'AL-Haram visitor', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle`
+--
+
+CREATE TABLE `vehicle` (
+  `id` int(20) NOT NULL,
+  `VehicleType` varchar(7) NOT NULL,
+  `Vehiclestatus` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`id`, `VehicleType`, `Vehiclestatus`) VALUES
+(1, 'Single', 'booked'),
+(30, 'Single', 'Avaliable'),
+(31, 'Single', 'Avaliable'),
+(32, 'Single', 'Avaliable'),
+(33, 'Single', 'Avaliable'),
+(34, 'Single', 'Avaliable'),
+(35, 'Single', 'Avaliable'),
+(36, 'Double', 'Avaliable'),
+(37, 'Double', 'Avaliable'),
+(38, 'Double', 'Avaliable');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `adminpram`
+--
+ALTER TABLE `adminpram`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vehicleId` (`vehicleId`),
+  ADD KEY `visitorId` (`visitorId`);
 
 --
 -- Indexes for table `users`
@@ -55,16 +143,52 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `adminpram`
+--
+ALTER TABLE `adminpram`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`vehicleId`) REFERENCES `vehicle` (`id`),
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`visitorId`) REFERENCES `users` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
