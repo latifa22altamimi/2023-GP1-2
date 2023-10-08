760ce49e881a4bbfa0ffa1ff7d1d4e95ac937a3e
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2023-10-08 15:21:40 +0300

    Add files via upload

---
+++ rehaab/details.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+include 'connect.php';
+$id=40;
+$RID =1;
+$sql = "SELECT * FROM reservation JOIN vehicle ON reservation.vehicleId = vehicle.id WHERE reservation.visitorId= $id AND reservation.id =$RID";
+$result = $conn->query($sql);
+ $ro2 = mysqli_fetch_assoc($result);
+      
+  
+  echo json_encode($ro2);
+
