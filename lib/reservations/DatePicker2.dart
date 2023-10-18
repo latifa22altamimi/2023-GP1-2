44262fde000a77327fb61a0e438979f027f9fbf4
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2023-10-18 22:41:14 +0300

    Update DatePicker2.dart

--- lib/reservations/DatePicker2.dart
+++ lib/reservations/DatePicker2.dart
@@ -11,7 +11,7 @@ class DatePicker2 extends GetxController{
   getDate({TextEditingController? controller, required BuildContext c,    String title="Reservation date"}) async {
 
     DateTime? pickedDate = await showDatePicker(
-      context: Get.context!,
+      context: c,
       initialDate: DateTime.now(),
       firstDate:DateTime.now(),
       lastDate:DateTime.now().add(const Duration(days: 90)),
