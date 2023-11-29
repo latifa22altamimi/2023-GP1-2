1b39a9f4f658d21a25f310b3fe893e779aa09c4c
latifa <latifah.maltamimi@gmailcom>
2023-11-30 02:32:22 +0300

    checkout update

--- lib/CheckOut/CheckOut.dart
+++ lib/CheckOut/CheckOut.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import "package:http/http.dart" as http;
 import 'package:location/location.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:rehaab/GlobalValues.dart';
 import 'package:stop_watch_timer/stop_watch_timer.dart';
 
@@ -20,6 +21,7 @@ class CheckOutState extends State<CheckOut> with TickerProviderStateMixin {
   final StopWatchTimer _stopWatchTimer = StopWatchTimer();
   final stopwatch = Stopwatch();
   String? finalTime;
+  var isFar;
 
   Future TawafTime() async {
     var url = "http://10.0.2.2/phpfiles/TawafDuration.php";
@@ -75,8 +77,63 @@ class CheckOutState extends State<CheckOut> with TickerProviderStateMixin {
         l = d(position.latitude, position.longitude, currentLocation.latitude,
                 currentLocation.longitude)
             .floor();
-        if (x > 50 || l > 50) {
-          print("far");
+        isFar = d(kaaba_lat, kaaba_lon, currentLocation.latitude,
+                currentLocation.longitude)
+            .floor();
+        if (isFar > 150 && isFar < 200) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                Future.delayed(Duration(seconds: 10), () {});
+                return Dialog(
+                  backgroundColor: Color.fromARGB(255, 247, 247, 247),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Lottie.asset('assets/images/warn.json',
+                            width: 100, height: 100),
+                        Text(
+                          'Success',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          " Warning! I noticed that you are approaching the exit of the Tawaf area. Please make sure that you have completed your circumambulation around the Kaaba before leaving",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ConstrainedBox(
+                              constraints: BoxConstraints.tightFor(
+                                  height: 38, width: 100),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              });
+        } else if (isFar > 50) {
           dispose();
           _stopWatchTimer.onStopTimer();
           Tawaf_time = (stopwatch.elapsed.inMilliseconds / 1000 / 60).ceil();
