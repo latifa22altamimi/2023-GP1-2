161c6d7f8103c55fb4decee84649165601603634
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2023-12-01 03:52:31 +0300

    Update reservationdetails.dart

--- lib/reservations/reservationdetails.dart
+++ lib/reservations/reservationdetails.dart
@@ -149,7 +149,7 @@ class _ReservationDetailsState extends State<ReservationDetails>
   /* bool start() {
     datetime = date! + " " + time!.substring(0, 5) + ":00";
     final d= DateTime.parse(datetime).difference(DateTime.now());
-      if(d.inMinutes<=30 && d.inMinutes>=0 ){  // "check in" button appears before 30 minutes of reservation time
+      if(Status == "Confirmed" && d.inMinutes<=30 && d.inMinutes>=0 ){  // "check in" button appears before 30 minutes of reservation time
       return true;
     } else {
       return false;
