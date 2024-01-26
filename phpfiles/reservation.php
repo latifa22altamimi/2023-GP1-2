ba6ede41ebe9b0ff84bdf47b70156af5a86c5755
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2024-01-26 16:30:40 +0300

    Update reservation.php

--- phpfiles/reservation.php
+++ phpfiles/reservation.php
@@ -6,12 +6,13 @@ $time= $_POST['time'];
 $VehicleType= $_POST['VehicleType'];
 $DrivingType= $_POST['DrivingType'];
 $DriverGender= $_POST['DriverGender'];
-
+$visitorName= $_POST['visitorName'];
+$Vnumber=$_POST['Vnumber'];
 $slots = "SELECT slotId FROM timeslots WHERE time='$time'";
 $slotIdResult =  mysqli_query($conn, $slots);
 $row = mysqli_fetch_assoc($slotIdResult);
 $slotId = $row['slotId'];
 
-$s ="INSERT INTO reservation (date,VehicleType,drivingType,driverGender,Status,visitorId,slotId) VALUES ('".$date."','".$VehicleType."','".$DrivingType."','".$DriverGender."','Confirmed','".$id."','".$slotId."')";
+$s ="INSERT INTO reservation (date,VehicleType,drivingType,driverGender,Status,userId,visitorName,VphoneNumber,slotId) VALUES ('".$date."','".$VehicleType."','".$DrivingType."','".$DriverGender."','Confirmed','".$id."','".$visitorName."','".$Vnumber."','".$slotId."')";
 
 $result2 = mysqli_query($conn, $s);
