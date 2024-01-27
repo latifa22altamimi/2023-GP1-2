ce7a24a6104d072f57d829f5c5a98129856abce9
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2024-01-27 23:24:11 +0300

    Add files via upload

---
+++ phpfiles/walkInDetails.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+include 'connect.php';
+$sql ="SELECT * FROM reservation";
+$result = $conn->query($sql);
+ while($ro2 = mysqli_fetch_assoc($result)){
+     $data[]=$ro2;
+ }
+      
+  
+  echo json_encode($data);
