bcbbbc7fc1f459e6cc31ea4ecf47e43afd4f5364
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2024-02-07 22:25:58 +0300

    Update checkin.dart

--- lib/checkIn/checkin.dart
+++ lib/checkIn/checkin.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:developer';
 
@@ -6,6 +7,8 @@ import 'package:flutter/material.dart';
 import '../customization/clip.dart';
 import '../widgets/constants.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:encrypt/encrypt.dart' as enc;
+import 'package:http/http.dart' as http;
 
 class CheckIn extends StatefulWidget {
   @override
@@ -13,6 +16,19 @@ class CheckIn extends StatefulWidget {
 }
 
 class _CheckInState extends State<CheckIn> {
+  StartTawaf() async {
+    final key = enc.Key.fromUtf8("3159a027584ad57a42c03d5dab118f68");
+  final iv = enc.IV.fromUtf8("e0c2ed4fbc3e1fb6");
+
+  final encrypter = enc.Encrypter(enc.AES(key, mode: enc.AESMode.cbc));
+  final decrypted = encrypter.decrypt(barcode!.code as enc.Encrypted, iv: iv);
+    var url = "http://10.0.2.2/phpfiles/startTawaf.php";
+    final res = await http.post(Uri.parse(url), body: {
+      "Rid": decrypted,
+    });
+    var respo = json.decode(res.body);
+    print(respo);
+  }
   
   Barcode? barcode;
   QRViewController? controller;
@@ -39,6 +55,8 @@ class _CheckInState extends State<CheckIn> {
 
   Widget build(BuildContext context) => SafeArea(
     child: Scaffold(
+     backgroundColor: kPrimaryLightColor,
+
       appBar: AppBar(
         leading: Container(
           padding: EdgeInsets.only(top: 5.0, bottom: 60.0),
@@ -66,44 +84,51 @@ class _CheckInState extends State<CheckIn> {
         ),
       ),
       body: Stack(
-        alignment:Alignment.center ,
+        alignment:AlignmentDirectional.topCenter ,
         children: <Widget>[
           buildQrView(context), 
-          Positioned(child: buildResult(), bottom: 10,)
+          
         ],)
     ));
   
   Widget buildResult()=> Container(
-    padding: EdgeInsets.all(12),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(8),
-      color: Colors.white24
-    ),
+    padding: EdgeInsets.all(20),
     child: Text( 
     barcode!= null? 'Result: ${barcode!.code}': 'Scan a code' ,
     maxLines: 10,
     )
   );
 
-   Widget buildQrView(BuildContext context) => QRView(key: qrKey,
-      onQRViewCreated: onQRViewCreated,
+   Widget buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
       overlay: QrScannerOverlayShape(
           borderColor: kPrimaryColor,
           borderRadius: 10,
           borderLength: 30,
           borderWidth: 10,
-          cutOutSize: MediaQuery.of(context).size.width*0.8),
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),);
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-    
-     
-  
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
 
-  void onQRViewCreated(QRViewController controller) {
-    setState(() => this.controller = controller);
-    
-    controller.scannedDataStream.listen((barcode) => setState(() => this.barcode = barcode));
-     
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        barcode = scanData;
+      });
+    });
   }
 
   void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
@@ -120,4 +145,5 @@ class _CheckInState extends State<CheckIn> {
     controller?.dispose();
     super.dispose();
   }
+
 }
