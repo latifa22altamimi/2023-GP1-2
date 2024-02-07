1b8e44fcf849ff45d315e2ee4ccaeb94a427eff8
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2024-02-08 00:06:43 +0300

    Update startTawaf.php

--- phpfiles/startTawaf.php
+++ phpfiles/startTawaf.php
@@ -7,8 +7,13 @@
 
 include 'connect.php';
 $Rid=$_POST['Rid'];
+$select1= "SELECT Status FROM reservation WHERE reservationId=$Rid";
+$result1= mysqli_query($conn, $select);
+$row = mysqli_fetch_assoc($result1);
+if($row["Status"]=="Confirmed"){
 $select= "UPDATE `reservation` SET `Status`='Active' WHERE reservationId=$Rid";
 $result= mysqli_query($conn, $select);
+}
 if($result){
     echo json_encode("started successfully");
     
