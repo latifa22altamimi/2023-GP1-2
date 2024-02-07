2dd8749b0cec84405ceb193d37a7223b81b11a54
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2024-02-08 00:06:08 +0300

    Update checkin.dart

--- lib/checkIn/checkin.dart
+++ lib/checkIn/checkin.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:developer';
+import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import '../customization/clip.dart';
 import '../widgets/constants.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
@@ -16,6 +18,7 @@ class CheckIn extends StatefulWidget {
 }
 
 class _CheckInState extends State<CheckIn> {
+  bool isVisibleSuccess=false;
   StartTawaf() async {
     final key = enc.Key.fromUtf8("3159a027584ad57a42c03d5dab118f68");
   final iv = enc.IV.fromUtf8("e0c2ed4fbc3e1fb6");
@@ -28,6 +31,9 @@ class _CheckInState extends State<CheckIn> {
     });
     var respo = json.decode(res.body);
     print(respo);
+    if(respo=="started successfully"){
+      isVisibleSuccess=true;
+    }
   }
   
   Barcode? barcode;
@@ -87,7 +93,155 @@ class _CheckInState extends State<CheckIn> {
         alignment:AlignmentDirectional.topCenter ,
         children: <Widget>[
           buildQrView(context), 
-          
+          Visibility(
+            visible: isVisibleSuccess,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                alignment: Alignment.center,
+                width: 500,
+                height: double.infinity,
+                color: Colors.transparent,
+                child: Stack(
+                  children: [
+                    BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
+                      child: Container(
+                        padding: EdgeInsets.all(50.0),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border:
+                              Border.all(color: Colors.white.withOpacity(0.13)),
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Colors.white.withOpacity(0.15),
+                                Colors.white.withOpacity(0.05),
+                              ])),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          /////////////////////////////////////////////////////
+          //Waiting list
+          Visibility(
+            visible: isVisibleSuccess,
+            
+            child: Dialog(
+                                                        backgroundColor:
+                                                            Color.fromARGB(255,
+                                                                247, 247, 247),
+                                                        shape: RoundedRectangleBorder(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        20)),
+                                                        child: Container(
+                                                          padding:
+                                                              const EdgeInsets
+                                                                  .all(20.0),
+                                                          child: Column(
+                                                            mainAxisAlignment:
+                                                                MainAxisAlignment
+                                                                    .center,
+                                                            mainAxisSize:
+                                                                MainAxisSize
+                                                                    .min,
+                                                            children: [
+                                                              Lottie.asset(
+                                                                  'assets/images/success.json',
+                                                                  width: 100,
+                                                                  height: 100),
+                                                              Text(
+                                                                'Success',
+                                                                style: TextStyle(
+                                                                    color: Colors
+                                                                        .black,
+                                                                    fontSize:
+                                                                        20,
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .w600),
+                                                              ),
+                                                              SizedBox(
+                                                                height: 10.0,
+                                                              ),
+                                                              Text(
+                                                                'ٌChecked in successfully',
+                                                                style: TextStyle(
+                                                                    color: Colors
+                                                                        .black,
+                                                                    fontSize:
+                                                                        17,
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .w400),
+                                                              ),
+                                                              SizedBox(
+                                                                height: 10.0,
+                                                              ),
+                                                              Row(
+                                                                crossAxisAlignment:
+                                                                    CrossAxisAlignment
+                                                                        .center,
+                                                                mainAxisSize:
+                                                                    MainAxisSize
+                                                                        .min,
+                                                                children: [
+                                                                  ConstrainedBox(
+                                                                    constraints: BoxConstraints.tightFor(
+                                                                        height:
+                                                                            38,
+                                                                        width:
+                                                                            100),
+                                                                  ),
+                                                                ],
+                                                              ),
+                                                              Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        //add to waiting list button
+
+                        ConstrainedBox(
+                          constraints:
+                              BoxConstraints.tightFor(height: 45, width: 120),
+                          child: ElevatedButton(
+                            onPressed: () {
+                          Navigator.of(context).pop();
+                         
+                              
+                            },
+                            child: Text(
+                              'Done',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromARGB(255, 60, 100, 73),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(50),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                                                            ],
+                                                          ),
+                                                        ),
+                                                      )
+          )
         ],)
     ));
   
@@ -111,7 +265,7 @@ class _CheckInState extends State<CheckIn> {
       key: qrKey,
       onQRViewCreated: _onQRViewCreated,
       overlay: QrScannerOverlayShape(
-          borderColor: kPrimaryColor,
+          borderColor: Colors.red,
           borderRadius: 10,
           borderLength: 30,
           borderWidth: 10,
