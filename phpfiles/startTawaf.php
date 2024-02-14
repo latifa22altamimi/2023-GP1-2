ff56772ada72376dad6ae3959c2af8b773ae0e7c
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2024-02-14 11:10:06 +0300

    Update startTawaf.php

--- phpfiles/startTawaf.php
+++ phpfiles/startTawaf.php
@@ -1,23 +1,25 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
 include 'connect.php';
-$Rid=$_POST['Rid'];
-$select1= "SELECT Status FROM reservation WHERE reservationId=$Rid";
-$result1= mysqli_query($conn, $select);
+
+$Rid = $_POST['Rid'];
+$select = "SELECT Status FROM reservation WHERE reservationId=$Rid";
+$result1 = mysqli_query($conn, $select);
 $row = mysqli_fetch_assoc($result1);
-if($row["Status"]=="Confirmed"){
-$select= "UPDATE `reservation` SET `Status`='Active' WHERE reservationId=$Rid";
-$result= mysqli_query($conn, $select);
+
+if ($row["Status"] == "Confirmed") {
+    $update = "UPDATE `reservation` SET `Status`='Active' WHERE reservationId=$Rid";
+    $result = mysqli_query($conn, $update);
+
+    if ($result) {
+        $response = json_encode("Tawaf started successfully");
+    } else {
+        $response = json_encode("Error in starting Tawaf");
+    }
+} else {
+    $response = json_encode("Reservation status is not Confirmed");
 }
-if($result){
-    echo json_encode("started successfully");
-    
-}else{
-    echo json_encode("Error in starting Tawaf");
-}
-?>
+
+echo $response;
+
+
