e221a8244aae72d34a3afaca6c18637eeb1ec62f
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2024-03-20 21:57:50 +0300

    Update splash_screen.dart

--- lib/main/splash_screen.dart
+++ lib/main/splash_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:rehaab/GlobalValues.dart';
+import 'package:rehaab/main/ManagerHome.dart';
+import 'package:rehaab/main/home.dart';
 import 'package:rehaab/main/onboardingscreen.dart';
-
+//import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -11,13 +14,40 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  Future<void> main() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? id  = prefs.getString('id');
+    final String? type= prefs.getString('type');
+    final String? Fullname=prefs.getString('name');
+    if(Fullname!=null && id!=null &&type!=null){
+GlobalValues.Fullname=Fullname;
+    GlobalValues.id=id;
+    GlobalValues.type=type;
+    }
+    
+ if(id==null || id.isEmpty) {
+  Timer(  const Duration(seconds: 3),
+   ()=>   Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const OnboardingScreen())));
+    } else if(id.isNotEmpty && type=='Vehicle manager') {
+Timer(  const Duration(seconds: 3),()=>
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => ManagerHome())));
+        
+    }
+    else if(id.isNotEmpty && type=='Al-haram visitor') {
+      Timer(const Duration(seconds: 3),()=>
+
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => home())));
+        
+    }
+  }
   @override
   void initState() {
     super.initState();
-    Timer(
-        const Duration(seconds: 3),
-        () => Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const OnboardingScreen())));
+    main();
+    
   }
 
   @override
@@ -45,7 +75,7 @@ class _SplashScreenState extends State<SplashScreen> {
                     'في رحاب الله',
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      color: Color.fromARGB(255, 47, 76, 58),
+                     color: Color.fromARGB(255, 47, 76, 58),
                       fontWeight: FontWeight.bold,
                       fontSize: 18,
                     ),
