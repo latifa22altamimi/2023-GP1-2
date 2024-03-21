38ed2f56bd7f4ef6094e0053e33da97ee802d517
Manal Alyami <107778339+ManalAlyami7@users.noreply.github.com>
2024-03-21 18:56:41 +0300

    Update connect.php

--- phpfiles/connect.php
+++ phpfiles/connect.php
@@ -5,7 +5,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
 
-$conn = new mysqli('localhost', 'root', '', 'rehaab');
+$conn = new mysqli('localhost', 'u271614468_reehab_team', 'AAa123123_', 'u271614468_Rehaab');
 
 // Check connection
 if ($conn->connect_error) {
