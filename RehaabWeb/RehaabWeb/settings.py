389ad938011cf17a7f72ed7e181ba6294fff2dfa
ffati <alnaserfatimah344@gmail.com>
2024-03-22 05:04:13 +0300

    web database hosting

--- RehaabWeb/RehaabWeb/settings.py
+++ RehaabWeb/RehaabWeb/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'RehaabWeb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rehaab',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'NAME': 'u271614468_rehaab',
+        'USER': 'u271614468_rehaab_team',
+        'PASSWORD': 'AAa123123_',
+        'HOST': '193.203.184.47',
         'PORT': '3306',
     }
 }
