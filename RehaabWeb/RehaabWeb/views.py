8b815503ac6bce7b6c0b8ddf89545018df77613a
ffati <alnaserfatimah344@gmail.com>
2024-05-12 04:06:10 +0300

    heatmap code

--- RehaabWeb/RehaabWeb/views.py
+++ RehaabWeb/RehaabWeb/views.py
@@ -467,19 +467,19 @@ def HeatMap(request):
     from ultralytics import YOLO
     model = YOLO('yolov8x.pt')
     video_filename = 'IMG_1352.MP4'
-    videopath = os.path.join(os.path.dirname(file), 'static', 'RehaabWeb', video_filename)
+    videopath = os.path.join(os.path.dirname(__file__), 'static', 'RehaabWeb', video_filename)
     cap = cv2.VideoCapture(videopath)
     track_history = defaultdict(lambda: [])
     last_positions = {}
     def calculate_distance(p1, p2):
-     return np.sqrt((p1[0] - p2[0])  2 + (p1[1] - p2[1])  2)
+     return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
     heatmap = np.zeros((int(cap.get(4)), int(cap.get(3)), 3), dtype=np.float32)
     width = int(cap.get(3))
     height = int(cap.get(4))
-    output_video_filename = 'output.mp4'
+    output_video_filename = 'output.avi'
     output_video_path = os.path.join(settings.BASE_DIR, 'RehaabWeb\\static\\RehaabWeb', output_video_filename)
     print(output_video_path)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_video_path, fourcc, 20.0, (width, height))
     while cap.isOpened():
      success, frame = cap.read()
